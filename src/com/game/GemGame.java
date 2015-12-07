54cf0588ffed5e04685435e57c7231504e64cf18
huangjeff <1qaz@2wsx>
2015-12-07 23:00:57 +0800

    Initial Source Code

---
+++ src/com/game/GemGame.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.game;
+
+/**
+ * 寶石連連看遊戲主程式
+ * @author jeff
+ * @date 2015/12/07
+ */
+public class GemGame {
+
+	/**
+	 * 
+	 */
+	public GemGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
